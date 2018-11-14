6b7701f26a2635c977ff5f330e86a70ca1093e39
americanman <snowboardracer7@gmail.com>
2018-11-14 15:18:26 -0500

    Add middleware

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests(for example validation happens in middleware)
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening in.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', (req, res) => {
     res.json({ message: 'hooray! welcome to our api!' });   
