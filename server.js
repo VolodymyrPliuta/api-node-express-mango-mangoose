66a4fd7de647971bf005e15d9287de4a0deb6a26
americanman <snowboardracer7@gmail.com>
2018-11-14 12:44:17 -0500

    Revamp argument/function to ES6

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
